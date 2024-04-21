9b93e0cd3380567349718e425440e5a5a6e0cecd
srivanesh <srivaneshbe@gmail.com>
2024-04-21 12:10:07 -0500

    Median with odd test case

--- src/com/core/Day1/MeanMedianDay1.java
+++ src/com/core/Day1/MeanMedianDay1.java
@@ -1,22 +1,31 @@
-package com.core;
+package com.core.Day1;
 
 import java.util.Arrays;
 
-public class MeanMedian {
+public class MeanMedianDay1 {
     public static void main(String args[])
     {
-        int a[] = { 1, 3, 4, 2, 7, 5, 8, 6 };
+        int a[] = { 1, 3, 4, 2, 7, 5, 8, 6 }; // even input
         int n = a.length;
 
         // Function call
         System.out.println("Mean = " + findMean(a, n));
         System.out.println("Median = " + findMedian(a, n));
+        System.out.println("Odd input ");
+        int b[] = { 9, 3, 1, 7, 5};
+        int c = b.length;
+        System.out.println("Odd input median = " + findMedian(b, c));
+
     }
 
     private static double findMedian(int[] a, int n) {
+        System.out.println("Input :"+Arrays.toString(a));
         Arrays.sort(a);
+        System.out.println("Sorted :"+Arrays.toString(a));
 
-        if(n % 2 !=0) return (double) a[n/2];
+        if(n % 2 !=0){
+            return (double) a[n/2];
+        }
 
         return (double) (a[n/2] + a[(n-1)/2])/ 2.0;
     }
